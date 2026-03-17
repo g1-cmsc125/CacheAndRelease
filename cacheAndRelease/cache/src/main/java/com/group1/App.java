6e03725f4d3df64453e4c8a2d5337b0de4ce633d
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-03-17 13:12:03 +0800

    initial build with resources

--- cacheAndRelease/cache/src/main/java/com/group1/App.java
+++ cacheAndRelease/cache/src/main/java/com/group1/App.java
@@ -1,17 +1,35 @@
 package com.group1;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+
+        URL htmlUrl = getClass().getResource("/web/index.html");
+        
+        if (htmlUrl != null) {
+            webEngine.load(htmlUrl.toExternalForm());
+        } else {
+            System.err.println("Critical Error: Core web assets could not be allocated.");
+        }
+
+        Scene scene = new Scene(webView, 1024, 768);
+        primaryStage.setTitle("QuantumTick CPU Scheduling Simulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
---
+++ cacheAndRelease/cache/src/main/java/com/group1/Launcher.java
@@ -0,0 +1,8 @@
+package com.group1;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Delegates execution to the actual JavaFX Application
+        App.main(args);
+    }
+}
